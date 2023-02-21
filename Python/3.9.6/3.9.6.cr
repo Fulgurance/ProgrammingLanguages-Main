20010233a1415a8f1d728852ecbe3f3831fe2514
fulgurance <zohran.londais@gmail.com>
2023-02-21 13:49:26 +0000

    Patched Perl and Python to support Pass option

--- Python/3.9.6/3.9.6.cr
+++ Python/3.9.6/3.9.6.cr
@@ -2,28 +2,37 @@ class Target < ISM::Software
     
     def configure
         super
-        configureSource([   "--prefix=/usr",
-                            "--enable-shared",
-                            "--with-system-expat",
-                            "--with-system-ffi",
-                            "--with-ensurepip=yes",
-                            "--enable-optimizations"],
-                            buildDirectoryPath)
+
+        if option("Pass1")
+            configureSource([   "--prefix=/usr",
+                                "--enable-shared",
+                                "--without-ensurepip"],
+                                buildDirectoryPath)
+        else
+            configureSource([   "--prefix=/usr",
+                                "--enable-shared",
+                                "--with-system-expat",
+                                "--with-system-ffi",
+                                "--with-ensurepip=yes",
+                                "--enable-optimizations"],
+                                buildDirectoryPath)
+        end
     end
     
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
     
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-    end
 
-    def install
-        super
-        makeLink("python3","#{Ism.settings.rootPath}/usr/bin/python",:symbolicLink)
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if !option("Pass1")
+            makeLink("python3","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/bin/python",:symbolicLink)
+        end
     end
 
 end
