6b5eff9d89fad0dabc69a3f3612f9c4aa27f40fd
fulgurance <zohran.londais@gmail.com>
2023-05-26 20:16:25 +0100

    Removed obsolete instructions

--- Crystal/1.7.2/1.7.2.cr
+++ Crystal/1.7.2/1.7.2.cr
@@ -1,23 +1,17 @@
 class Target < ISM::Software
     
-    def extract
-        super
-
-        moveFile("#{workDirectoryPath(false)}/crystal-1.7.2","#{workDirectoryPath(false)}/1.7.2")
-    end
-    
     def build
         super
 
         makeSource( [Ism.settings.makeOptions],
                     buildDirectoryPath,
-                    {"PATH" => "$PATH:#{workDirectoryPath}/crystal-1.7.2-1/bin"})
+                    {"PATH" => "$PATH:#{workDirectoryPath}/Crystal-Compiler-1.7.2/bin"})
     end
     
     def prepareInstallation
         super
 
-        makeSource([Ism.settings.makeOptions,"PREFIX=/usr","DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["PREFIX=/usr","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
 end
