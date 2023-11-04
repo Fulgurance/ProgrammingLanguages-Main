c476418d0dc76b94de03d8e96ec0cc38d61fbd9d
Fulgurance <zohran.londais@gmail.com>
2023-11-04 21:30:20 +0000

    Added new crystal version

---
+++ Crystal/1.10.1/1.10.1.cr
@@ -0,0 +1,16 @@
+class Target < ISM::Software
+    
+    def build
+        super
+
+        makeSource( path: buildDirectoryPath,
+                    environment: {"PATH" => "$PATH:#{workDirectoryPath}/Crystal-Compiler-1.10.1/bin"})
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["PREFIX=/usr","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
