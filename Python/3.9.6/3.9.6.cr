147fcc9e4020cf82c035857f6c97f6a854c94567
Fulgurance <zohran.londais@gmail.com>
2024-05-05 14:18:00 +0100

    Updated symlink recording method

--- Python/3.9.6/3.9.6.cr
+++ Python/3.9.6/3.9.6.cr
@@ -29,13 +29,9 @@ class Target < ISM::Software
         super
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-    end
-
-    def install
-        super
 
         if !option("Pass1")
-            makeLink("python3","#{Ism.settings.rootPath}usr/bin/python",:symbolicLink)
+            makeLink("python3","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/python",:symbolicLink)
         end
     end
 
