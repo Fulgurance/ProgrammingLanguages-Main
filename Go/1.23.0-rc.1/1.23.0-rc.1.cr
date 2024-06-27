cb91a362f7fafdcae1a19981319e51754ed60f18
Fulgurance <zohran.londais@gmail.com>
2024-06-27 11:17:27 +0100

    Added go language

---
+++ Go/1.23.0-rc.1/1.23.0-rc.1.cr
@@ -0,0 +1,27 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runFile(file:       "src/all.bash",
+                path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        exit 1
+    end
+
+end
