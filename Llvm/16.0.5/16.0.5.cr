0f6ca17ebfa562ec977ab7f511c9144c9a7eb10b
Fulgurance <zohran.londais@gmail.com>
2024-09-28 23:40:48 +0100

    Added support for cross compilation

--- Llvm/16.0.5/16.0.5.cr
+++ Llvm/16.0.5/16.0.5.cr
@@ -18,17 +18,18 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:      "-DCMAKE_INSTALL_PREFIX=/usr                \
-                                        -DLLVM_ENABLE_FFI=ON                        \
-                                        -DCMAKE_BUILD_TYPE=Release                  \
-                                        -DLLVM_BUILD_LLVM_DYLIB=ON                  \
-                                        -DLLVM_LINK_LLVM_DYLIB=ON                   \
-                                        -DLLVM_ENABLE_RTTI=ON                       \
-                                        -DLLVM_TARGETS_TO_BUILD=\"host;AMDGPU;BPF\" \
-                                        -DLLVM_BINUTILS_INCDIR=/usr/include         \
-                                        -DLLVM_INCLUDE_BENCHMARKS=OFF               \
-                                        -DLLVM_ENABLE_LIBCXX=ON                     \
-                                        -DCLANG_DEFAULT_PIE_ON_LINUX=ON             \
+        runCmakeCommand(arguments:      "-DCMAKE_INSTALL_PREFIX=/usr                    \
+                                        -DLLVM_HOST_TRIPLE=#{Ism.settings.systemTarget} \
+                                        -DLLVM_ENABLE_FFI=ON                            \
+                                        -DCMAKE_BUILD_TYPE=Release                      \
+                                        -DLLVM_BUILD_LLVM_DYLIB=ON                      \
+                                        -DLLVM_LINK_LLVM_DYLIB=ON                       \
+                                        -DLLVM_ENABLE_RTTI=ON                           \
+                                        -DLLVM_TARGETS_TO_BUILD=\"host;AMDGPU;BPF\"     \
+                                        -DLLVM_BINUTILS_INCDIR=/usr/include             \
+                                        -DLLVM_INCLUDE_BENCHMARKS=OFF                   \
+                                        -DLLVM_ENABLE_LIBCXX=ON                         \
+                                        -DCLANG_DEFAULT_PIE_ON_LINUX=ON                 \
                                         -Wno-dev -G Ninja ..",
                         path:           buildDirectoryPath,
                         environment:    {"CC" => "gcc","CXX" => "g++"})
