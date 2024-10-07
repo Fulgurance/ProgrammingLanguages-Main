60f6ecbad4191e4a98754f1af32077eef0db85fa
Fulgurance <zohran.londais@gmail.com>
2024-10-07 18:11:57 +0100

    Updated installation process

--- Rustc/1.71.1/1.71.1.cr
+++ Rustc/1.71.1/1.71.1.cr
@@ -67,7 +67,7 @@ class Target < ISM::Software
         docs = false
         extended = true
         locked-deps = true
-        tools = ["cargo", "clippy", "rustdoc", "rustfmt"]
+        tools = ["cargo", "xargo", "clippy", "rustdoc", "rustfmt"]
         vendor = true
 
         [install]
