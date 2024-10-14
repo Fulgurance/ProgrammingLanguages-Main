e94b1090da257525677ea355009a13bc6b7ee4d8
Fulgurance <zohran.londais@gmail.com>
2024-10-14 10:29:52 +0100

    Added experimental patch

--- Rust/1.71.1/1.71.1.cr
+++ Rust/1.71.1/1.71.1.cr
@@ -63,7 +63,7 @@ class Target < ISM::Software
         link-shared = true
 
         [build]
-        target = ["#{Ism.settings.systemTarget}"]
+        target = ["x86_64-unknown-linux-gnu","#{Ism.settings.systemTarget}"]
         docs = false
         extended = true
         locked-deps = true
@@ -77,6 +77,9 @@ class Target < ISM::Software
         [rust]
         channel = "stable"
 
+        [target.x86_64-unknown-linux-gnu]
+        llvm-config = "/usr/bin/llvm-config"
+
         [target.#{Ism.settings.systemTarget}]
         llvm-config = "/usr/bin/llvm-config"
         CODE
@@ -86,7 +89,7 @@ class Target < ISM::Software
     def build
         super
 
-        runPythonCommand(   arguments:      "./x.py build --target #{Ism.settings.systemTarget}.json",
+        runPythonCommand(   arguments:      "./x.py build --target x86_64-unknown-linux-gnu,#{Ism.settings.systemTarget}.json",
                             path:           buildDirectoryPath,
                             environment:    {"LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
     end
@@ -94,7 +97,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        runPythonCommand(   arguments:      "./x.py install --target #{Ism.settings.systemTarget}.json",
+        runPythonCommand(   arguments:      "./x.py install --target x86_64-unknown-linux-gnu,#{Ism.settings.systemTarget}.json",
                             path:           buildDirectoryPath,
                             environment:    {   "DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}",
                                                 "LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
