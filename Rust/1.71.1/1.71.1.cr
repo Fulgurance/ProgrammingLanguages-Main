f0455716e0d810cdcd1c5be1ab0faed42f75fb86
Fulgurance <zohran.londais@gmail.com>
2024-10-16 19:28:09 +0100

    Experimental patch

--- Rust/1.71.1/1.71.1.cr
+++ Rust/1.71.1/1.71.1.cr
@@ -3,57 +3,57 @@ class Target < ISM::Software
     def prepare
         super
 
-        # targetData = <<-CODE
-        # {
-        #     "arch": "x86_64",
-        #     "cpu": "x86-64",
-        #     "crt-static-respected": true,
-        #     "data-layout": "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128",
-        #     "dynamic-linking": true,
-        #     "env": "gnu",
-        #     "has-rpath": true,
-        #     "has-thread-local": true,
-        #     "is-builtin": false,
-        #     "linker-flavor": "gcc",
-        #     "llvm-target": "#{Ism.settings.systemTarget}",
-        #     "max-atomic-width": 64,
-        #     "os": "linux",
-        #     "position-independent-executables": true,
-        #     "pre-link-args": {
-        #         "gcc": [
-        #         "-m64"
-        #         ],
-        #         "ld": [
-        #         "-m64"
-        #         ]
-        #     },
-        #     "relro-level": "full",
-        #     "stack-probes": {
-        #         "kind": "inline"
-        #     },
-        #     "static-position-independent-executables": true,
-        #     "supported-sanitizers": [
-        #         "address",
-        #         "leak",
-        #         "memory",
-        #         "thread",
-        #         "cfi",
-        #         "kcfi"
-        #     ],
-        #     "supported-split-debuginfo": [
-        #         "packed",
-        #         "unpacked",
-        #         "off"
-        #     ],
-        #     "supports-xray": true,
-        #     "target-family": [
-        #         "unix"
-        #     ],
-        #     "target-pointer-width": "64"
-        # }
-        #
-        # CODE
-        # fileWriteData("#{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",targetData)
+        targetData = <<-CODE
+        {
+            "arch": "x86_64",
+            "cpu": "x86-64",
+            "crt-static-respected": true,
+            "data-layout": "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128",
+            "dynamic-linking": true,
+            "env": "gnu",
+            "has-rpath": true,
+            "has-thread-local": true,
+            "is-builtin": false,
+            "linker-flavor": "gcc",
+            "llvm-target": "#{Ism.settings.systemTarget}",
+            "max-atomic-width": 64,
+            "os": "linux",
+            "position-independent-executables": true,
+            "pre-link-args": {
+                "gcc": [
+                "-m64"
+                ],
+                "ld": [
+                "-m64"
+                ]
+            },
+            "relro-level": "full",
+            "stack-probes": {
+                "kind": "inline"
+            },
+            "static-position-independent-executables": true,
+            "supported-sanitizers": [
+                "address",
+                "leak",
+                "memory",
+                "thread",
+                "cfi",
+                "kcfi"
+            ],
+            "supported-split-debuginfo": [
+                "packed",
+                "unpacked",
+                "off"
+            ],
+            "supports-xray": true,
+            "target-family": [
+                "unix"
+            ],
+            "target-pointer-width": "64"
+        }
+
+        CODE
+        fileWriteData("#{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",targetData)
 
         copyFile(   "#{buildDirectoryPath}/compiler/rustc_target/src/spec/x86_64_unknown_linux_gnu.rs",
                     "#{buildDirectoryPath}/compiler/rustc_target/src/spec/#{Ism.settings.systemTarget.gsub("-","_")}.rs")
@@ -93,7 +93,7 @@ class Target < ISM::Software
     def build
         super
 
-        runPythonCommand(   arguments:      "./x.py build",
+        runPythonCommand(   arguments:      "./x.py build --host #{Ism.settings.systemTarget} --target #{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",
                             path:           buildDirectoryPath,
                             environment:    {"LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
     end
@@ -101,7 +101,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        runPythonCommand(   arguments:      "./x.py install",
+        runPythonCommand(   arguments:      "./x.py install --host #{Ism.settings.systemTarget} --target #{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",
                             path:           buildDirectoryPath,
                             environment:    {   "DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}",
                                                 "LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
