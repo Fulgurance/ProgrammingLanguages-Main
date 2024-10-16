109b2915b48bfb0b84a5fc654499a42715e7c590
Fulgurance <zohran.londais@gmail.com>
2024-10-16 13:04:00 +0100

    Added experimental patch for proper target support

--- Rust/1.71.1/1.71.1.cr
+++ Rust/1.71.1/1.71.1.cr
@@ -3,57 +3,64 @@ class Target < ISM::Software
     def prepare
         super
 
-        targetData = <<-CODE
-        {
-            "arch": "x86_64",
-            "cpu": "x86-64",
-            "crt-static-respected": true,
-            "data-layout": "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128",
-            "dynamic-linking": true,
-            "env": "gnu",
-            "has-rpath": true,
-            "has-thread-local": true,
-            "is-builtin": false,
-            "linker-flavor": "gcc",
-            "llvm-target": "#{Ism.settings.systemTarget}",
-            "max-atomic-width": 64,
-            "os": "linux",
-            "position-independent-executables": true,
-            "pre-link-args": {
-                "gcc": [
-                "-m64"
-                ],
-                "ld": [
-                "-m64"
-                ]
-            },
-            "relro-level": "full",
-            "stack-probes": {
-                "kind": "inline"
-            },
-            "static-position-independent-executables": true,
-            "supported-sanitizers": [
-                "address",
-                "leak",
-                "memory",
-                "thread",
-                "cfi",
-                "kcfi"
-            ],
-            "supported-split-debuginfo": [
-                "packed",
-                "unpacked",
-                "off"
-            ],
-            "supports-xray": true,
-            "target-family": [
-                "unix"
-            ],
-            "target-pointer-width": "64"
-        }
+        # targetData = <<-CODE
+        # {
+        #     "arch": "x86_64",
+        #     "cpu": "x86-64",
+        #     "crt-static-respected": true,
+        #     "data-layout": "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128",
+        #     "dynamic-linking": true,
+        #     "env": "gnu",
+        #     "has-rpath": true,
+        #     "has-thread-local": true,
+        #     "is-builtin": false,
+        #     "linker-flavor": "gcc",
+        #     "llvm-target": "#{Ism.settings.systemTarget}",
+        #     "max-atomic-width": 64,
+        #     "os": "linux",
+        #     "position-independent-executables": true,
+        #     "pre-link-args": {
+        #         "gcc": [
+        #         "-m64"
+        #         ],
+        #         "ld": [
+        #         "-m64"
+        #         ]
+        #     },
+        #     "relro-level": "full",
+        #     "stack-probes": {
+        #         "kind": "inline"
+        #     },
+        #     "static-position-independent-executables": true,
+        #     "supported-sanitizers": [
+        #         "address",
+        #         "leak",
+        #         "memory",
+        #         "thread",
+        #         "cfi",
+        #         "kcfi"
+        #     ],
+        #     "supported-split-debuginfo": [
+        #         "packed",
+        #         "unpacked",
+        #         "off"
+        #     ],
+        #     "supports-xray": true,
+        #     "target-family": [
+        #         "unix"
+        #     ],
+        #     "target-pointer-width": "64"
+        # }
+        #
+        # CODE
+        # fileWriteData("#{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",targetData)
 
-        CODE
-        fileWriteData("#{buildDirectoryPath}/#{Ism.settings.systemTarget}.json",targetData)
+        copyFile(   "#{buildDirectoryPath}/compiler/rustc_target/src/spec/x86_64_unknown_linux_gnu.rs",
+                    "#{buildDirectoryPath}/compiler/rustc_target/src/spec/#{Ism.settings.systemTarget.gsub("-","_")}.rs")
+
+        fileReplaceText(path: "#{buildDirectoryPath}/compiler/rustc_target/src/spec/#{Ism.settings.systemTarget.gsub("-","_")}.rs",
+                        text: "x86_64-unknown-linux-gnu",
+                        newText: "#{Ism.settings.systemTarget}")
 
         configData = <<-CODE
         changelog-seen = 2
@@ -63,7 +70,7 @@ class Target < ISM::Software
         link-shared = true
 
         [build]
-        target = ["x86_64-unknown-linux-gnu","#{Ism.settings.systemTarget}"]
+        target = ["#{Ism.settings.systemTarget}"]
         docs = false
         extended = true
         locked-deps = true
@@ -77,9 +84,6 @@ class Target < ISM::Software
         [rust]
         channel = "stable"
 
-        [target.x86_64-unknown-linux-gnu]
-        llvm-config = "/usr/bin/llvm-config"
-
         [target.#{Ism.settings.systemTarget}]
         llvm-config = "/usr/bin/llvm-config"
         CODE
@@ -89,7 +93,7 @@ class Target < ISM::Software
     def build
         super
 
-        runPythonCommand(   arguments:      "./x.py build --target x86_64-unknown-linux-gnu,#{Ism.settings.systemTarget}.json",
+        runPythonCommand(   arguments:      "./x.py build",
                             path:           buildDirectoryPath,
                             environment:    {"LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
     end
@@ -97,7 +101,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        runPythonCommand(   arguments:      "./x.py install --target x86_64-unknown-linux-gnu,#{Ism.settings.systemTarget}.json",
+        runPythonCommand(   arguments:      "./x.py install",
                             path:           buildDirectoryPath,
                             environment:    {   "DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}",
                                                 "LIBSSH2_SYS_USE_PKG_CONFIG" => "1"})
