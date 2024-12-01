ad49c8ebaad8a0d6bf95e438159a78b64aa332e5
Fulgurance <zohran.londais@gmail.com>
2024-12-01 21:02:31 +0000

    Added vala language

---
+++ Vala/0.9.8/0.9.8.cr
@@ -0,0 +1,23 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource(arguments:  "--prefix=/usr",
+                        path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
