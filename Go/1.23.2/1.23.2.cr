4e22123e57c6d16c1d6739004f9e453c17b00065
Fulgurance <zohran.londais@gmail.com>
2024-12-04 14:22:19 +0000

    Updated installation process

--- Go/1.23.2/1.23.2.cr
+++ Go/1.23.2/1.23.2.cr
@@ -6,17 +6,16 @@ class Target < ISM::Software
         super
     end
     
-    def configure
-        super
-
-        runFile(file:       "all.bash",
-                path:       buildDirectoryPath)
-    end
-    
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        runFile(file:       "all.bash",
+                path:       buildDirectoryPath,
+                environment:    {   "GOARCH" => "amd64",
+                                    "GOAMD64" => "v1",
+                                    "GOROOT_FINAL" => "/usr/lib/go",
+                                    "GOROOT_BOOTSTRAP" => "/usr/lib/go",
+                                    "PATH" => "#{workDirectoryPath}/Go-Compiler-#{version}/bin:$PATH"})
     end
     
     def prepareInstallation
