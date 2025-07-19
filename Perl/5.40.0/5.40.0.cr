d3cfa93c0798bb096f6cdf8e5aee381f9355c2a1
Fulgurance <zohran.londais@gmail.com>
2025-07-19 16:54:01 +0100

    Added necessary flag to build with musl

--- Perl/5.40.0/5.40.0.cr
+++ Perl/5.40.0/5.40.0.cr
@@ -5,34 +5,36 @@ class Target < ISM::Software
 
         if option("Pass1")
             runFile(file:       "Configure",
-                    arguments:  "-des                                           \
-                                -Dprefix=/usr                                   \
-                                -Dvendorprefix=/usr                             \
-                                -Duseshrplib                                    \
+                    arguments:  "-des                                                               \
+                                -Dprefix=/usr                                                       \
+                                -Dvendorprefix=/usr                                                 \
+                                -Duseshrplib                                                        \
                                 -Dprivlib=/usr/lib/perl#{majorVersion}/#{version}/core_perl         \
                                 -Darchlib=/usr/lib/perl#{majorVersion}/#{version}/core_perl         \
                                 -Dsitelib=/usr/lib/perl#{majorVersion}/#{version}/site_perl         \
                                 -Dsitearch=/usr/lib/perl#{majorVersion}/#{version}/site_perl        \
                                 -Dvendorlib=/usr/lib/perl#{majorVersion}/#{version}/vendor_perl     \
                                 -Dvendorarch=/usr/lib/perl#{majorVersion}/#{version}/vendor_perl    \
-                                -Dman1dir=/usr/share/man/man1                   \
-                                -Dman3dir=/usr/share/man/man3",
+                                -Dman1dir=/usr/share/man/man1                                       \
+                                -Dman3dir=/usr/share/man/man3                                       \
+                                -Duseithreads",
                     path:       buildDirectoryPath)
         else
             runFile(file:           "Configure",
-                    arguments:      "-des                                           \
-                                    -Dprefix=/usr                                   \
-                                    -Dvendorprefix=/usr                             \
+                    arguments:      "-des                                                               \
+                                    -Dprefix=/usr                                                       \
+                                    -Dvendorprefix=/usr                                                 \
                                     -Dprivlib=/usr/lib/perl#{majorVersion}/#{version}/core_perl         \
                                     -Darchlib=/usr/lib/perl#{majorVersion}/#{version}/core_perl         \
                                     -Dsitelib=/usr/lib/perl#{majorVersion}/#{version}/site_perl         \
                                     -Dsitearch=/usr/lib/perl#{majorVersion}/#{version}/site_perl        \
                                     -Dvendorlib=/usr/lib/perl#{majorVersion}/#{version}/vendor_perl     \
                                     -Dvendorarch=/usr/lib/perl#{majorVersion}/#{version}/vendor_perl    \
-                                    -Dman1dir=/usr/share/man/man1                   \
-                                    -Dman3dir=/usr/share/man/man3                   \
-                                    -Dpager=\"/usr/bin/less -isR\"                  \
-                                    -Duseshrplib                                    \
+                                    -Dman1dir=/usr/share/man/man1                                       \
+                                    -Dman3dir=/usr/share/man/man3                                       \
+                                    -Dpager=\"/usr/bin/less -isR\"                                      \
+                                    -Duseshrplib                                                        \
+                                    -Duseithreads                                                       \
                                     -Dusethreads",
                     path:           buildDirectoryPath,
                     environment:    {"BUILD_ZLIB" => "False","BUILD_BZIP2" => "0"})
